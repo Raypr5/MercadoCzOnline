7ecdb7b629f74f4011c766b0b6e7fc188046001c
Jeliel Matheus <jelielmatheus10@gmail.com>
2022-04-06 20:27:56 -0300

    RF003 feito com sucesso

---
+++ src/main/java/br/edu/catolica/ppi/ProjetoMC/controller/TipoBoletoController.java
@@ -0,0 +1,43 @@
+package br.edu.catolica.ppi.ProjetoMC.controller;
+
+import br.edu.catolica.ppi.ProjetoMC.model.Cartao;
+import br.edu.catolica.ppi.ProjetoMC.model.TipoBoleto;
+import br.edu.catolica.ppi.ProjetoMC.service.TipoBoletoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/tipoBoleto")
+public class TipoBoletoController {
+
+    private final TipoBoletoService tipoBoletoService;
+
+    public TipoBoletoController(TipoBoletoService tipoBoletoService){
+        this.tipoBoletoService = tipoBoletoService;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity save(@RequestBody TipoBoleto tipoBoleto){
+        return ResponseEntity.ok()
+                .body(tipoBoletoService.saveOrUpdate(tipoBoleto));
+    }
+
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity getById(@PathVariable("id") UUID id){
+        return ResponseEntity.ok().body(tipoBoletoService.findById(id));
+    }
+
+    @GetMapping("/buscar")
+    public ResponseEntity findAll(){
+        return ResponseEntity.ok().body(tipoBoletoService.getAll());
+    }
+
+    @DeleteMapping
+    public ResponseEntity delete(@RequestBody TipoBoleto tipoBoleto){
+        tipoBoletoService.delete(tipoBoleto);
+        return ResponseEntity.noContent().build();
+    }
+}
