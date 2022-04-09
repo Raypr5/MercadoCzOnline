29b01bad31fb8371ada6389d8b7236306f43e9cb
Jeliel Matheus <jelielmatheus10@gmail.com>
2022-04-08 23:17:45 -0300

    Criação das classes Repository, Service e Controller do Tipo Cartão

---
+++ src/main/java/br/edu/catolica/ppi/ProjetoMC/service/TipoCartaoService.java
@@ -0,0 +1,40 @@
+package br.edu.catolica.ppi.ProjetoMC.service;
+
+import br.edu.catolica.ppi.ProjetoMC.model.Cartao;
+import br.edu.catolica.ppi.ProjetoMC.model.TipoCartao;
+import br.edu.catolica.ppi.ProjetoMC.repository.CartaoRepository;
+import br.edu.catolica.ppi.ProjetoMC.repository.TipoCartaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class TipoCartaoService {
+
+    @Autowired
+    TipoCartaoRepository tipoCartaoRepository;
+
+    public TipoCartao saveOrUpdate(TipoCartao tipoCartao){
+        return tipoCartaoRepository.save(tipoCartao);
+    }
+
+    public List<TipoCartao> getAllCartao(){
+        return tipoCartaoRepository.findAll();
+    }
+
+    public TipoCartao findById(UUID id){
+        return tipoCartaoRepository.findById(id).get();
+    }
+
+    public List<TipoCartao> getAll(){
+        return tipoCartaoRepository.findAll();
+    }
+
+    public void delete(TipoCartao tipoCartao){
+        TipoCartao c = findById(tipoCartao.getId());
+        tipoCartaoRepository.delete(c);
+    }
+}
